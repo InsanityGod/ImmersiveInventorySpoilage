98586dc29e66373082be7c9aa8d5c549665d8d28
_Kristof_ <krizziewissie@gmail.com>
2024-10-23 14:12:03 +0200

    Add project files.

---
+++ ImmersiveInventorySpoilage/HarmonyPatches/PatchInventoryBasePlayerClass.cs
@@ -0,0 +1,91 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Vintagestory.API.Common;
+using Vintagestory.API.Common.Entities;
+using Vintagestory.API.MathTools;
+using Vintagestory.Common;
+using Vintagestory.GameContent;
+
+namespace ImmersiveInventorySpoilage.HarmonyPatches
+{
+    public static class PatchInventoryBasePlayerClass
+    {
+        public static void PostFix(InventoryBasePlayer __instance)
+        {
+            __instance.OnAcquireTransitionSpeed = (EnumTransitionType transType, ItemStack stack, float baseMul) =>
+            {
+                if (__instance.Player == null)
+                {
+                    return baseMul;
+                }
+
+                float positionAwarePerishRate = 1;
+
+                if (__instance.Api != null && transType == EnumTransitionType.Perish)
+                {
+                    //START GetPerishRate
+
+                    var pos = __instance.Player?.Entity?.Pos?.AsBlockPos ?? __instance.Pos;
+
+                    if (pos != null)
+                    {
+                        BlockPos sealevelpos = pos.Copy();
+                        sealevelpos.Y = __instance.Api.World.SeaLevel;
+
+                        //TODO maybe cache this value for a few ticks for performance
+                        float temperature = __instance.Api.World.BlockAccessor.GetClimateAt(sealevelpos, EnumGetClimateMode.ForSuppliedDate_TemperatureOnly, __instance.Api.World.Calendar.TotalDays).Temperature;
+
+                        //TODO maybe cache this value for a few ticks for performance
+                        var room = __instance.Api.ModLoader.GetModSystem<RoomRegistry>().GetRoomForPosition(pos);
+
+                        float soilTempWeight = 0f;
+                        float skyLightProportion = (float)room.SkylightCount / (float)Math.Max(1, room.SkylightCount + room.NonSkylightCount);
+
+                        if (room.IsSmallRoom)
+                        {
+                            soilTempWeight = 1f;
+                            soilTempWeight -= 0.4f * skyLightProportion;
+                            soilTempWeight -= 0.5f * GameMath.Clamp((float)room.NonCoolingWallCount / (float)Math.Max(1, room.CoolingWallCount), 0f, 1f);
+                        }
+
+                        int lightlevel = __instance.Api.World.BlockAccessor.GetLightLevel(pos, EnumLightLevelType.OnlySunLight);
+                        float lightImportance = 0.1f;
+
+                        if (room.IsSmallRoom)
+                        {
+                            lightImportance += 0.3f * soilTempWeight + 1.75f * skyLightProportion;
+                        }
+                        else if ((float)room.ExitCount <= 0.1f * (float)(room.CoolingWallCount + room.NonCoolingWallCount))
+                        {
+                            lightImportance += 1.25f * skyLightProportion;
+                        }
+                        else
+                        {
+                            lightImportance += 0.5f * skyLightProportion;
+                        }
+
+                        lightImportance = GameMath.Clamp(lightImportance, 0f, 1.5f);
+
+                        float airTemp = temperature + (float)GameMath.Clamp(lightlevel - 11, 0, 10) * lightImportance;
+                        float cellarTemp = 5f;
+                        float hereTemp = GameMath.Lerp(airTemp, cellarTemp, soilTempWeight);
+                        hereTemp = Math.Min(hereTemp, airTemp);
+                        positionAwarePerishRate = Math.Max(0.1f, Math.Min(2.4f, (float)Math.Pow(3.0, (double)(hereTemp / 19f) - 1.2) - 0.1f));
+                    }
+                    //END GetPerishRate
+                }
+                else if (transType == EnumTransitionType.Dry || transType == EnumTransitionType.Melt)
+                {
+                    positionAwarePerishRate = 0.25f;
+                }
+
+                return baseMul * positionAwarePerishRate;
+            };
+        }
+    }
+}
---
+++ ImmersiveInventorySpoilage/ImmersiveInventorySpoilageModSystem.cs
@@ -0,0 +1,36 @@
+﻿using HarmonyLib;
+using ImmersiveInventorySpoilage.HarmonyPatches;
+using System;
+using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+using Vintagestory.API.Config;
+using Vintagestory.API.Server;
+using Vintagestory.Common;
+using Vintagestory.Server;
+
+namespace ImmersiveInventorySpoilage
+{
+    public class ImmersiveInventorySpoilageModSystem : ModSystem
+    {
+        private Harmony harmony;
+
+        // Called on server and client
+        // Useful for registering block/entity classes on both sides
+        public override void Start(ICoreAPI api)
+        {
+            if (!Harmony.HasAnyPatches(Mod.Info.ModID))
+            {
+                harmony = new Harmony(Mod.Info.ModID);
+                var invType = typeof(InventoryBasePlayer);
+
+                var invTypeCtor1 = AccessTools.Constructor(invType, new Type[] { typeof(string), typeof(string), typeof(ICoreAPI) });
+                harmony.Patch(invTypeCtor1, postfix: new HarmonyMethod(typeof(PatchInventoryBasePlayerClass).GetMethod(nameof(PatchInventoryBasePlayerClass.PostFix))));
+
+                var invTypeCtor2 = AccessTools.Constructor(invType, new Type[] { typeof(string), typeof(ICoreAPI) });
+                harmony.Patch(invTypeCtor2, postfix: new HarmonyMethod(typeof(PatchInventoryBasePlayerClass).GetMethod(nameof(PatchInventoryBasePlayerClass.PostFix))));
+            }
+        }
+
+        public override void Dispose() => harmony?.UnpatchAll(Mod.Info.ModID);
+    }
+}
