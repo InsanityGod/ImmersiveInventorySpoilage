a36493d3850543b09bc690f7bed82e2551ee2e77
_Kristof_ <krizziewissie@gmail.com>
2025-01-18 12:30:09 +0100

    slightly updated config/settings to add more AutoConfigLib integration

--- ImmersiveInventorySpoilage/Config/ModConfig.cs
+++ ImmersiveInventorySpoilage/Config/ModConfig.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
@@ -15,23 +16,27 @@ namespace ImmersiveInventorySpoilage.Config
         /// Must be between 0 and 1, 1 meaning that temperature/room/light affects the player inventory just as much as any other inventory (such as chests) while 0 means it isn't affected at all
         /// </summary>
         [Range(0f, 1f)]
+        [DefaultValue(1f)]
         public float PositionAwarePerishRateSimularity { get; set; } = 1;
 
         /// <summary>
         /// Whether the drying multiplier is allowed to go negative.
         /// If set to true drying process may start reversing if you get too wet.
         /// </summary>
+        [DefaultValue(true)]
         public bool AllowNegativeDryMultiplier { get; set; } = true;
 
         /// <summary>
         /// Wether item wetness is a thing
         /// If set to true, certain items (such as snow) may make you wet if you carry them
         /// </summary>
+        [DefaultValue(true)]
         public bool Feature_ItemWetness { get; set; } = true;
 
         /// <summary>
         /// Wether stuff (for now only slush) can continue to melt all the way until it turns into water (and slips through the backpack, effictively removing it from the inventory)
         /// </summary>
+        [DefaultValue(true)]
         public bool Feature_StuffCanFullyMelt { get; set; } = true;
 
         /// <summary>
